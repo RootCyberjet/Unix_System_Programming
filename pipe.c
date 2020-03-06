0ca8b7744c612e65055dab3b550afdff22e217c6
SHIBA BISWAS <56774836+RootCyberjet@users.noreply.github.com>
2020-03-07 01:28:42 +0530

    Simple example of pipe system call

---
+++ pipe.c
@@ -0,0 +1,42 @@
+#include<stdio.h>
+#include<sys/wait.h>
+#include<stdlib.h>
+#include<unistd.h>
+void main()
+{
+	int n, fd[2],stat;
+	char buf[100];
+
+	if(pipe(fd) == -1)
+	{
+		perror("PIPE");
+		exit(1);
+	}
+
+	switch(fork())
+	{
+		case -1:
+			{
+				printf("Forking error...\n");
+				exit(1);
+			}
+		case 0:
+			{
+				printf("I'm child process...\n");
+				close(fd[1]);
+				n = read(fd[0], buf, 100);
+				write(STDOUT_FILENO, buf, n);
+				exit(10);
+				break;
+			}
+		default:
+			{
+				wait(&stat);
+				printf("I'm parent process...\n");
+				printf("Child's exit status : %d\n",WEXITSTATUS(stat));
+				close(fd[0]);
+				write(fd[1], "Hey I'm RootCyberjet...\n", 24);
+			}
+	}
+	exit(0);
+}
